d5b74bca0d73bf3deed4c333a70a0cabd6ced1fe
frequ <samppa.valkama@pienipiiri.fi>
2013-05-06 17:03:02 +0300

    rewrote category scroller to be category slider

--- js/views/pregameview.js
+++ js/views/pregameview.js
@@ -12,10 +12,21 @@ var PreGameView = Backbone.View.extend({
 	render: function() {
 
         $('#header').show();
+        var split = [];
+        var txtSplit = [];
 
         var gameId = this.model.get('gameId');
 		if( gameId === 1 ){
             this.preload('kalat');
+            var categoryImg = Settings.get('categoryImg');
+            split = categoryImg.chunk(3);
+        }else if( gameId === 3){
+            var catImg = Settings.get('categories').titleImg;
+            var catTitle = Settings.get('categories').titles;
+            split = catImg.chunk(3);
+            txtSplit = catTitle.chunk(3);
+            console.log(txtSplit);
+            console.log(split);
         }else if ( gameId === 5){
             this.preloadAud('elaimet');
         }else if ( gameId === 6){
@@ -30,6 +41,14 @@ var PreGameView = Backbone.View.extend({
                          guide: this.model.get('guide'),
                          difficulty: Settings.get('difficulty'),
                          categoryImg: Settings.get('categoryImg'),
+                         slide1: split[0],
+                         slide1Titles: txtSplit[0],
+                         slide2: split[1],
+                         slide2Titles: txtSplit[1],
+                         slide3: split[2],
+                         slide3Titles: txtSplit[2],
+                         slide4: split[3],
+                         slide4Titles: txtSplit[3],
                          categories: Settings.get('categories').titles,
                          titleImg: Settings.get('categories').titleImg};
         console.log(variables);
@@ -37,82 +56,147 @@ var PreGameView = Backbone.View.extend({
 
 		this.$el.html(template);
 
+
+
         if( gameId === 1 ){
+            $('.clicker').hide();
+            $("#categoryCarousel").removeClass('hidden');
             var category = Settings.get('category');
+
+            $('.item img').addClass('category');
+
+            console.log(category);
             $('.categories').removeClass('hidden');
 
             if(category === 'dinosaurukset'){
-                $('.inner-img img:nth(0)').addClass('selected');
-                this.preload('dinosaurukset');
+                $('img.category:nth(0)').addClass('selected');
+                $('ol.carousel-indicators li:nth(0)').addClass('active');
+                $('.carousel-inner .item:nth(0)').addClass('active');
             }else if(category === 'elaimet'){
-                this.preload('elaimet');
-                $('.inner-img img:nth(1)').addClass('selected');
+                $('img.category:nth(1)').addClass('selected');
+                $('ol.carousel-indicators li:nth(0)').addClass('active');
+                $('.carousel-inner .item:nth(0)').addClass('active');
             }else if(category === 'hedelmat'){
-                this.preload('hedelmat');
-                $('.inner-img img:nth(2)').addClass('selected');
+                $('img.category:nth(2)').addClass('selected');
+                $('ol.carousel-indicators li:nth(0)').addClass('active');
+                $('.carousel-inner .item:nth(0)').addClass('active');
             }else if(category === 'kalat'){
-                this.preload('kalat');
-                $('.inner-img img:nth(3)').addClass('selected');
+                $('img.category:nth(3)').addClass('selected');
+                $('ol.carousel-indicators li:nth(1)').addClass('active');
+                $('.carousel-inner .item:nth(1)').addClass('active');
             }else if(category === 'kansallispuvut'){
-                this.preload('kansallispuvut');
-                $('.inner-img img:nth(4)').addClass('selected');
+                $('img.category:nth(4)').addClass('selected');
+                $('ol.carousel-indicators li:nth(1)').addClass('active');
+                $('.carousel-inner .item:nth(1)').addClass('active');
             }else if(category === 'kasvit'){
-                this.preload('kasvit');
-                $('.inner-img img:nth(5)').addClass('selected');
+                $('img.category:nth(5)').addClass('selected');
+                $('ol.carousel-indicators li:nth(1)').addClass('active');
+                $('.carousel-inner .item:nth(1)').addClass('active');
             }else if(category === 'linnut'){
-                this.preload('linnut');
-                $('.inner-img img:nth(6)').addClass('selected');
+                $('img.category:nth(6)').addClass('selected');
+                $('ol.carousel-indicators li:nth(2)').addClass('active');
+                $('.carousel-inner .item:nth(2)').addClass('active');
             }else if(category === 'liput'){
-                this.preload('liput');
-                $('.inner-img img:nth(7)').addClass('selected');
+                $('img.category:nth(7)').addClass('selected');
+                $('ol.carousel-indicators li:nth(2)').addClass('active');
+                $('.carousel-inner .item:nth(2)').addClass('active');
             }else if(category === 'soittimet'){
-                this.preload('soittimet');
-                $('.inner-img img:nth(8)').addClass('selected');
+                $('img.category:nth(8)').addClass('selected');
+                $('ol.carousel-indicators li:nth(2)').addClass('active');
+                $('.carousel-inner .item:nth(2)').addClass('active');
             }else if(category === 'tyokalut'){
-                this.preload('tyokalut');
-                $('.inner-img img:nth(9)').addClass('selected');
+                $('img.category:nth(9)').addClass('selected');
+                $('ol.carousel-indicators li:nth(3)').addClass('active');
+                $('.carousel-inner .item:nth(3)').addClass('active');
             }else if(category === 'urheiluvalineet'){
-                this.preload('urheiluvalineet');
-                $('.inner-img img:nth(10)').addClass('selected');
+                $('img.category:nth(10)').addClass('selected');
+                $('ol.carousel-indicators li:nth(3)').addClass('active');
+                $('.carousel-inner .item:nth(3)').addClass('active');
             }
+
         }else if ( gameId === 3){
             var textCat = Settings.get('textCategory');
-            $('.categories-text').removeClass('hidden');
+            $("#categoryCarousel").removeClass('hidden');
+            console.log(textCat);
+            $('.item img').addClass('text-category');
 
             if(textCat === 'eläimet'){
-                $('.inner-img-short img:nth(0)').addClass('selected')
+                $('img:nth(0)').addClass('selected');
+                $('ol.carousel-indicators li:nth(0)').addClass('active');
+                $('.carousel-inner .item:nth(0)').addClass('active');
             }else if(textCat === 'ammatit'){
-                $('.inner-img-short img:nth(1)').addClass('selected')
+                $('img:nth(1)').addClass('selected');
+                $('ol.carousel-indicators li:nth(0)').addClass('active');
+                $('.carousel-inner .item:nth(0)').addClass('active');
             }else if(textCat === 'kasvit'){
-                $('.inner-img-short img:nth(2)').addClass('selected')
+                $('img:nth(2)').addClass('selected');
+                $('ol.carousel-indicators li:nth(0)').addClass('active');
+                $('.carousel-inner .item:nth(0)').addClass('active');
             }else if(textCat === 'kaupungit'){
-                $('.inner-img-short img:nth(3)').addClass('selected')
+                $('img:nth(3)').addClass('selected');
+                $('ol.carousel-indicators li:nth(1)').addClass('active');
+                $('.carousel-inner .item:nth(1)').addClass('active');
             }else if(textCat === 'miesten nimet'){
-                $('.inner-img-short img:nth(4)').addClass('selected')
+                $('img:nth(4)').addClass('selected');
+                $('ol.carousel-indicators li:nth(1)').addClass('active');
+                $('.carousel-inner .item:nth(1)').addClass('active');
             }else if(textCat === 'naisten nimet'){
-                $('.inner-img-short img:nth(5)').addClass('selected')
+                $('img:nth(5)').addClass('selected');
+                $('ol.carousel-indicators li:nth(1)').addClass('active');
+                $('.carousel-inner .item:nth(1)').addClass('active');
             }else if(textCat === 'sisustus'){
-                $('.inner-img-short img:nth(6)').addClass('selected')
+                $('img:nth(6)').addClass('selected');
+                $('ol.carousel-indicators li:nth(2)').addClass('active');
+                $('.carousel-inner .item:nth(2)').addClass('active');
             }else if(textCat === 'soittimet'){
-                $('.inner-img-short img:nth(7)').addClass('selected')
+                $('img:nth(7)').addClass('selected');
+                $('ol.carousel-indicators li:nth(2)').addClass('active');
+                $('.carousel-inner .item:nth(2)').addClass('active');
             }else if(textCat === 'työkalut'){
-                $('.inner-img-short img:nth(8)').addClass('selected')
+                $('img:nth(8)').addClass('selected');
+                $('ol.carousel-indicators li:nth(2)').addClass('active');
+                $('.carousel-inner .item:nth(2)').addClass('active');
             }else if(textCat === 'urheilu'){
-                $('.inner-img-short img:nth(9)').addClass('selected')
+                $('img:nth(9)').addClass('selected');
+                $('ol.carousel-indicators li:nth(3)').addClass('active');
+                $('.carousel-inner .item:nth(3)').addClass('active');
             }else if(textCat === 'valtiot'){
-                $('.inner-img-short img:nth(10)').addClass('selected')
+                $('img:nth(10)').addClass('selected');
+                $('ol.carousel-indicators li:nth(3)').addClass('active');
+                $('.carousel-inner .item:nth(3)').addClass('active');
             }
 
         }
+
+        $("#categoryCarousel").carousel('pause');
+
+        $("#categoryCarousel").swipe({
+            swipeLeft:function(){
+                $("#categoryCarousel").carousel('next')
+            },
+            swipeRight:function(){
+                $("#categoryCarousel").carousel('prev')
+            }
+        });
 
 
         $("#content").imagesLoaded( function ( $images ){
                 console.log( $images.length + ' images have been loaded');
                 $(".container").removeClass('loading');
+
+        });
+       // $('.carousel-inner div:nth-child(1)').addClass('active');
+/*
+        $('.easy').click(function(){
+            scroller.scrollTo(500, 0, false);
         });
 
+        $('.medium').click(function(){
 
-
+            var val = scroller.getValues();
+            console.log(val);
+        });
+ */
 
         var diff = Settings.get('difficulty');
         if(diff === 'easy'){
@@ -132,11 +216,10 @@ var PreGameView = Backbone.View.extend({
         'click .preview' : 'previewVideo',
         'click #play-game' : 'play',
         'click .category' : 'selectCategory',
-        'click .text-category' : 'selectTextCategory'
+        'click .clicker' : 'selectTextCategory'
     },
 
     selectCategory: function () {
-
         $('.category').removeClass('selected');
         var target = $(event.target);
         target.toggleClass('selected');
@@ -147,18 +230,23 @@ var PreGameView = Backbone.View.extend({
 
     },
 
-    selectTextCategory: function () {
-        $('.text-category').removeClass('selected');
-        var target = $(event.target);
-        var index = target.index()-1;
-        var cat = $('span:nth-child('+index+')').text().toLowerCase();
 
+    selectTextCategory: function () {
+
+        $('.text-category').removeClass('selected');
+        var index = $(event.target).index();
+        if(index > 0){
+            index = index/2;
+        }
+
+        $('.active img.text-category:nth('+index+')').addClass('selected');
+        var cat = $('.active h4:nth('+index+')').text().toLowerCase();
         Settings.set({textCategory:cat});
-        console.log(Settings.get('textCategory'));
-        target.toggleClass('selected');
+
     },
 
     easySelected: function() {
+
         $('.easy').addClass('btn-success');
         $('.medium').removeClass('btn-warning');
         $('.hard').removeClass('btn-danger');
